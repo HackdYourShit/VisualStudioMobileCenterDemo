4197e66fd10785c035c96269e8945509d59f9415
Vineet Choudhary <vineetchoudhary@live.in>
2016-11-24 13:08:29 +0530

    Initial Commit

---
+++ VisualStudioMobileCenterDemo/MSVC-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  MSVC-Bridging-Header.m
+//  VisualStudioMobileCenterDemo
+//
+//  Created by Vineet Choudhary on 24/11/16.
+//  Copyright © 2016 Developer Insider. All rights reserved.
+//
+
+#import "MSAlertController.h"
